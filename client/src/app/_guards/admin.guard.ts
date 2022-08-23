0fe930684a9521491d27f78e4f4371b806c64c6d
Resa Gary <resa.gary@yahoo.com>
2022-08-22 21:22:06 -0700

    Added Identity and role management

--- client/src/app/_guards/admin.guard.ts
+++ client/src/app/_guards/admin.guard.ts
@@ -8,14 +8,16 @@ import { AccountService } from '../_services/account.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AdminGuard implements CanActivate {
   constructor(private accountService: AccountService, private toastr: ToastrService) {}
-  
+
   canActivate(): Observable<boolean> {
     return this.accountService.currentUser$.pipe(
       map(user => {
-        if (user) return true;
-        this.toastr.error("You are unauthorized to view content.");
+        if (user.roles.includes('Admin') || user.roles.includes('Moderator')) {
+          return true;
+        }
+        this.toastr.error('You are unauthorized to view this content.');
       })
     );
   }
